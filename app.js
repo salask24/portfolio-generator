b37d489e2afe5388bec66cf4eebc537d2eef40b9
Kimberly Salas <salask0104@gmail.com>
2021-05-12 09:32:11 -0700

    added fs to the top of the page and ran commands that created html with my name and profile (index.html)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const name = profileDataArgs[0];
 const github = profileDataArgs[1];
-const fs = require('fs');
 
 const generatePage = (name, github) => {
     return `
