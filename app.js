afeb5227a55f3e9a9c52109d8fbe61164f0b0220
Kimberly Salas <salask0104@gmail.com>
2021-05-11 18:38:49 -0700

    recoded to created a multi-line input

---
+++ app.js
@@ -0,0 +1,23 @@
+const profileDataArgs = process.argv.slice(2);
+const generatePage = (userName, githubName) => {
+    return `
+      Name: ${userName}
+      GitHub: ${githubName}
+    `;
+};
+console.log(generatePage('Jane', 'janehub'));
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
