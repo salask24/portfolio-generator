61cd6eb49f53daa02d90d432156fdc8d23dbd1a6
Kimberly Salas <salask0104@gmail.com>
2021-05-12 09:51:00 -0700

    fixed files

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
+const generatePage = require('./src/page-template')
+
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
-
-const generatePage = require('./src/page-template.js')
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
